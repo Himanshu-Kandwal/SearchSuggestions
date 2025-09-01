d19edddc926844fafe012ec4a962acca4437067f
Himanshu-Kandwal <hkandwal50@gmail.com>
2025-09-02 00:14:06 +0530

    add : TTL LRU cache

---
+++ library/src/main/kotlin/cache/InMemoryTTLLRUCache.kt
@@ -0,0 +1,40 @@
+package cache
+
+class InMemoryTTLLRUCache(private val cacheConfig: CacheConfig = CacheConfig()) : SuggestionCache<String, CacheItem> {
+
+    private val cache = object : LinkedHashMap<String, CacheItem>(cacheConfig.cacheSize, 0.75f, true) {
+        override fun removeEldestEntry(eldest: MutableMap.MutableEntry<String, CacheItem>?): Boolean { //remove the eldest entry when this cache size is bigger than configured cache size
+            return size > cacheConfig.cacheSize
+        }
+    }
+
+    @Synchronized
+    override fun get(key: String): CacheItem? {
+        val item = cache[key]
+        item?.let {
+            if (isExpired(it)) return null
+            else return it
+        }
+        return null
+    }
+
+    @Synchronized
+    override fun put(key: String, suggestions: CacheItem) {
+        cache[key] = suggestions
+    }
+
+    @Synchronized
+    override fun delete(key: String): String? {
+        return if (cache.remove(key) != null) key else null
+    }
+
+    @Synchronized
+    override fun contains(key: String): Boolean {
+        return cache.containsKey(key)
+    }
+
+    private fun isExpired(item: CacheItem): Boolean {
+        val now = System.currentTimeMillis()
+        return (now - item.insertedAt) > cacheConfig.cacheTTL
+    }
+}
